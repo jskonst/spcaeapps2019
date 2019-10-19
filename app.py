0022287c40efb9e74721e17fe1e7cddf53260078
jskonst <jskonst@yandex.ru>
2019-10-19 22:30:14 +0300

    latest changes

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
 from flask import Flask, escape, request, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+from pymongo import MongoClient
+client = MongoClient('mongodb://%s:%s@127.0.0.1:27017' % ("root", "example"))
+db = client["users"]
+coll = db["users"]
 
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    coll = db["test"]
+    return jsonify({"res": coll})
 
-@app.route('/face', methods=['POST'])
-def face():
-    search = request.json.get("id")
-    name = request.json.get("photoBase64")
+
+@app.route('/create', methods=['POST'])
+def create_user():
+    photo = request.json.get("photoBase64")
+    name = request.json.get("name")
+    user = {
+        "photoBase64": photo,
+        "name": name
+    }
+    usr = coll.insert_one(user)
     return jsonify({"result":"ok"})
+
+
+@app.route('/check', methods=['POST'])
+def get_user():
+    photo = request.json.get("photoBase64")
+    user = {
+        "photoBase64": photo
+    }
+    dbUser = coll.find_one({"photoBase64": photo})
+    if dbUser:
+        return jsonify({"result": dbUser.name})
+    else: 
+        return jsonify({"error": "user not found"})
